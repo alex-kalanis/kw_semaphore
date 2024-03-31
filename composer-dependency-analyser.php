eb42c334b7c5fcf982c00550d2b675fb22bdb527
Petr Plsek <me@kalanys.com>
2024-03-31 14:19:03 +0200

    kw_semaphore - dependency paths ignore 2

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -14,4 +14,6 @@ return $config
     // ignore errors on specific packages and paths
     ->ignoreErrorsOnPackageAndPath('alex-kalanis/kw_storage', __DIR__ . '/php-src/Semaphore/Storage.php', [ErrorType::DEV_DEPENDENCY_IN_PROD])
     ->ignoreErrorsOnPackageAndPath('alex-kalanis/kw_files', __DIR__ . '/php-src/Semaphore/Files.php', [ErrorType::DEV_DEPENDENCY_IN_PROD])
+    ->ignoreErrorsOnPackageAndPath('alex-kalanis/kw_storage', __DIR__ . '/php-src/Semaphore/Factory.php', [ErrorType::DEV_DEPENDENCY_IN_PROD])
+    ->ignoreErrorsOnPackageAndPath('alex-kalanis/kw_files', __DIR__ . '/php-src/Semaphore/Factory.php', [ErrorType::DEV_DEPENDENCY_IN_PROD])
 ;
